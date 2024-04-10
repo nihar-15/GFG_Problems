cae99511a9fa03c432eaf4f62a268f734b1bf2b3
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-04-10 22:56:23 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximize dot product/maximize-dot-product.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n = sc.nextInt();
+                    int m = sc.nextInt();
+                    int a[] = new int[n];
+                    int b[] = new int[m];
+                    for(int i = 0;i<n;i++)
+                        a[i] = sc.nextInt();
+                    for(int i = 0;i<m;i++)
+                        b[i] = sc.nextInt();    
+                    Solution ob = new Solution();
+                    System.out.println(ob.maxDotProduct(n, m, a, b));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    
+	public int maxDotProduct(int m, int n, int A[], int B[]) 
+	{ 
+		// Your code goes here
+		
+		
+	
+    }
+}
