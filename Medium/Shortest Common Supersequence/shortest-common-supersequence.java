9dcbddc5faf5b8f82af54c519ee39dfedfa278a5
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-05-11 15:42:11 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Shortest Common Supersequence/shortest-common-supersequence.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+	    
+	    //taking input using Scanner class
+		Scanner sc=new Scanner(System.in);
+		
+		//taking total testcases
+		int t=sc.nextInt();
+		
+		sc.nextLine();
+		while(t-->0)
+		{
+		   //taking String X and Y
+		   String S[]=sc.nextLine().split(" ");
+		   String X=S[0];
+		   String Y=S[1];
+		   
+		   //calling function shortestCommonSupersequence()
+		   System.out.println(new Solution().shortestCommonSupersequence(X, Y, X.length(), Y.length()));
+		}
+	}
+
+
+
+
+       
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find length of shortest common supersequence of two strings.
+    public static int shortestCommonSupersequence(String X,String Y,int m,int n)
+    {
+        //Your code here
+        
+        int lcsLength = lcs(X, Y);
+        return (m - lcsLength)+ (n - lcsLength)+ lcsLength;
+    }
+    
+
+    
+    
+    public static  int lcs(String text1 , String text2){
+          int n = text1.length();
+     int m = text2.length();
+     int dp[][] = new int[n][m];
+     for(int a[] :dp){
+        Arrays.fill(a , -1);
+     }
+    return  func(text1 , text2 , n-1, m-1, dp);
+
+        }
+       static  int func(String text1 , String text2, int n , int m, int dp[][]){
+            if(n <0 || m<0){
+                return 0;
+            }
+            if(dp[n][m] != -1){
+                return dp[n][m];
+            }
+            if(text1.charAt(n) == text2.charAt(m)){
+                return dp[n][m] = 1+ func(text1 , text2 , n-1 , m-1 , dp);
+            }
+            return dp[n][m]= 0 + Math.max(func(text1 , text2 ,n-1 , m ,dp) , func(text1 , text2 , n , m-1 , dp));
+        }
+    
+
+}
