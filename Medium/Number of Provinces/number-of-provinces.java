716b7474342994e95772839bb6026e3398317ba7
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-05-17 16:01:40 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Number of Provinces/number-of-provinces.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int V = Integer.parseInt(read.readLine());
+            
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            
+            for(int i=0; i<V; i++)
+            {
+                String S[] = read.readLine().split(" ");
+                ArrayList<Integer> temp = new ArrayList<>();
+                for(int j=0; j<V; j++)
+                    temp.add(Integer.parseInt(S[j]));
+                adj.add(temp);
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.numProvinces(adj,V));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int numProvinces(ArrayList<ArrayList<Integer>> adj, int V) {
+        // code here
+        boolean[] visited = new boolean[V];
+        int count = 0;
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                dfs(adj, visited, i, V);
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    static void dfs(ArrayList<ArrayList<Integer>> adj, boolean[] visited, int node, int V) {
+        visited[node] = true;
+
+        for (int i = 0; i < V; i++) {
+            if (adj.get(node).get(i) == 1 && !visited[i]) {
+                dfs(adj, visited, i, V);
+            }
+        }
+    }
+};
