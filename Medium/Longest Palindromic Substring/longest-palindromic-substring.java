811df10ddb1cf08aa73aaa94923b795669a66494
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-05-23 06:20:12 -0400

    Added Solution - GfG to GitHub

---
+++ Medium/Longest Palindromic Substring/longest-palindromic-substring.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            
+            String S = read.readLine().trim();
+            Solution ob = new Solution();
+            System.out.println(ob.longestPalindrome(S));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    String longestPalindrome(String s){
+         if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i); // Odd length palindromes
+            int len2 = expandAroundCenter(s, i, i + 1); // Even length palindromes
+            int len = Math.max(len1, len2);
+            if (len > end - start+1) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
