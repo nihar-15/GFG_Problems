e29e570537cab4652f48aa92dbfda62dd75645bc
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-06-05 10:59:19 -0400

    Added Solution - GfG to GitHub

---
+++ Medium/Shortest Job first/shortest-job-first.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    while (T-- > 0) {
+      int n = sc.nextInt();
+      int a[] = new int[n];
+      for(int i=0;i<n;i++){
+        a[i]=sc.nextInt();
+      }
+      Solution obj = new Solution();
+      int ans = obj.solve(a);
+      System.out.println(ans);
+    }
+  }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int solve(int bt[] ) {
+    // code here
+Arrays.sort(bt);
+int w=0,s=0;
+for(int a:bt){
+    s+=a;
+    w+=s-a;}
+return w/bt.length;
+  }
+}
+     
