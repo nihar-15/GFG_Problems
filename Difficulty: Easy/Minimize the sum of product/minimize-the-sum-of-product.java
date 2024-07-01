75bb3ab55282860c58bb1e629ffba254553d5600
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-07-01 05:58:04 -0400

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimize the sum of product/minimize-the-sum-of-product.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+import java.util.stream.*;
+
+// Position this line where user code will be pasted.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            long b[] = new long[(int)(n)];
+
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                b[i] = Long.parseLong(inputLine1[i]);
+            }
+
+            Solution obj = new Solution();
+            System.out.println(obj.minValue(a, b, n));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+
+    public long minValue(long a[], long b[], long n) {
+        // Your code goes here
+           Arrays.sort(a);
+        Arrays.sort(b);
+        long sum = 0;
+        for(long i =0;i<n;i++){
+            sum += a[(int)i] *b[(int)(n-1-i)];
+        }
+        return sum;
+    }
+}
