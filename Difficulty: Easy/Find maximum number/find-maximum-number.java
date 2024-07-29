70a7eeaaa787d47ab7c8289b3e6d87f58d1808bb
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-07-30 00:19:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find maximum number/find-maximum-number.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String N = read.readLine();
+
+            Solution ob = new Solution();
+            System.out.println(ob.findMax(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static String findMax(String N) {
+        // code here
+        
+        char ch[] = N.toCharArray();
+        Arrays.sort(ch);
+        String s ="";
+        for(int i = N.length()-1; i >=0 ; i--){
+            s += ch[i];
+        }
+        return s;
+    }
+};
