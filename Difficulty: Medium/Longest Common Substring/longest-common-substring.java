8d18365abccfe2c660536b2a03d867f80754e32a
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-08-14 22:49:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.java
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.java
@@ -1,54 +1,45 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
 import java.io.*;
 import java.util.*;
 
-class GFG
-{
-    public static void main(String args[])throws IOException
-    {
+class GFG {
+    public static void main(String args[]) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
-        while(t-- > 0)
-        {
-            String input[] = read.readLine().trim().split(" ");
-            int n = Integer.parseInt(input[0]);
-            int m = Integer.parseInt(input[1]);
-            
+        while (t-- > 0) {
             String S1 = read.readLine().trim();
             String S2 = read.readLine().trim();
 
             Solution ob = new Solution();
-            System.out.println(ob.longestCommonSubstr(S1, S2, n, m));
+            System.out.println(ob.longestCommonSubstr(S1, S2));
         }
     }
 }
 // } Driver Code Ends
 
 
-//User function Template for Java
+// User function Template for Java
 
-class Solution{
-    int longestCommonSubstr(String S1, String S2, int n, int m){
+class Solution {
+    public int longestCommonSubstr(String str1, String str2) {
         // code here
-        
-           int[][] dp=new int[n+1][m+1];
-        
-        int lcs=0;
-        
-        for(int i=0;i<=n;i++)
-        {
-            for(int j=0;j<=m;j++)
-            {
-                if(i==0||j==0) dp[i][j]=0;
-                else if(S1.charAt(i-1)==S2.charAt(j-1)){
-                    dp[i][j]=1+dp[i-1][j-1];
-                    lcs=Math.max(lcs,dp[i][j]);
+        int m = str1.length();
+        int n = str2.length();
+        int ans = 0;
+        int dp[][] = new int[m+1][n+1];
+        for(int i = 1; i <= m ; i++){
+            for(int j =1; j<=n ; j++){
+                if(str1.charAt(i - 1) == str2.charAt(j -1)){
+                    dp[i][j] = 1+ dp[i-1][j-1];
+                    ans = Math.max(ans , dp[i][j]);
+                }else{
+                    dp[i][j] = 0;
                 }
-                else dp[i][j]=0;
             }
         }
-        return lcs;
+         
+          return ans; 
     }
 }
