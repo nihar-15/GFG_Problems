e1d2635b613639bb1f146936465a60cde8ebcb32
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-09-25 12:07:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.java
@@ -0,0 +1,108 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Driver_code {
+    static Node insert(Node head, int data) {
+        Node temp = new Node(data);
+        if (head == null) {
+            head = temp;
+            return head;
+        } else {
+            Node t = head;
+            while (t.next != null) {
+                t = t.next;
+            }
+            t.next = temp;
+        }
+        return head;
+    }
+
+    static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+
+            Node head = null;
+
+            String str[] = read.readLine().trim().split(" ");
+            int listSize = str.length;
+            for (int i = 0; i < listSize; i++) {
+                head = insert(head, Integer.parseInt(str[i]));
+            }
+            boolean f = new Solution().isPalindrome(head);
+
+            System.out.println(f ? "true" : "false");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/* Structure of class Node is
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}*/
+
+class Solution {
+    // Function to check whether the list is palindrome.
+    boolean isPalindrome(Node head) {
+      
+        int n=0;
+        Node size=head;
+        while(size!=null){
+            size=size.next;
+            n++;
+        }
+        
+        Node trav=head;
+        int[] arr=new int[n];
+        int i=0;
+        while(trav!=null){
+            arr[i]=trav.data;
+            trav=trav.next;
+            i++;
+        }
+        
+        while(head!=null){
+            if(head.data!=arr[i-1])return false;
+            
+            head=head.next;
+            i--;
+        }
+        
+        return true;
+}
+    
+}
