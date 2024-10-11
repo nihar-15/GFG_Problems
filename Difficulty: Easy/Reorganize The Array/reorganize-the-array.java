6d7313cbc1aa83138236c504affaedc33e17cf2c
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-10-11 10:14:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reorganize The Array/reorganize-the-array.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+// } Driver Code Ends
+class Solution {
+    public List<Integer> rearrange(List<Integer> arr) {
+        // Code here\
+        
+        List<Integer> ans = new ArrayList<>();
+        for(int i = 0 ; i< arr.size() ;i++){
+            ans.add(-1);
+        }
+        for(int i = 0 ; i < arr.size() ; i++){
+            if(arr.get(i) != -1){
+                ans.set(arr.get(i) , arr.get(i));
+            }
+        }
+        return ans;
+    }
+}
+
+//{ Driver Code Starts.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
+        Solution solution = new Solution();
+
+        while (t-- > 0) {
+            String input = scanner.nextLine();
+            String[] inputArr = input.split("\\s+");
+            List<Integer> arr = new ArrayList<>();
+            for (String s : inputArr) {
+                arr.add(Integer.parseInt(s));
+            }
+
+            List<Integer> ans = solution.rearrange(arr);
+
+            for (int num : ans) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
+// } Driver Code Ends
