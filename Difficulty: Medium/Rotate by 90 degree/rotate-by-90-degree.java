816f3acc84b245251c94b68ee56537b3d32c6f44
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-11-05 21:02:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotate by 90 degree/rotate-by-90-degree.java
+++ Difficulty: Medium/Rotate by 90 degree/rotate-by-90-degree.java
@@ -1,75 +1,57 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
-import java.util.*; 
 import java.io.*;
 import java.lang.*;
+import java.util.*;
 
-class DriverClass
-{
-	public static void main(String[] args) 
-	{ 
-	    Scanner sc = new Scanner(System.in);
+class DriverClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        
-        while (t-- > 0)
-        {
+
+        while (t-- > 0) {
             int n = sc.nextInt();
             int[][] arr = new int[n][n];
-            
+
             for (int i = 0; i < n; i++)
-                for (int j = 0; j < n; j++)
-                    arr[i][j] = sc.nextInt();
-            
+                for (int j = 0; j < n; j++) arr[i][j] = sc.nextInt();
+
             GFG g = new GFG();
             g.rotate(arr);
-            printMatrix (arr);
+            printMatrix(arr);
+
+            System.out.println("~");
         }
-	} 
-	
-	static void printMatrix(int arr[][]) 
-	{ 
-		for (int i = 0; i < arr.length; i++) { 
-			for (int j = 0; j < arr[0].length; j++) 
-				System.out.print(arr[i][j] + " "); 
-			System.out.println(""); 
-		} 
-	} 
+    }
+
+    static void printMatrix(int arr[][]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) System.out.print(arr[i][j] + " ");
+            System.out.println("");
+        }
+    }
 }
 // } Driver Code Ends
+
+
 
 
 //User function Template for Java
 
 class GFG
 {
-    static void rotate(int matrix[][]) 
+    static void rotate(int mat[][]) 
     {
-        int n = matrix.length;
-       transpose(n , matrix);
-        reverse(matrix);
-    }
-  static void transpose(int n,int a[][])
-    {
-        for(int i =0 ; i < n; i++){
-            for(int j = i+1 ; j< n ; j++){
-               int   temp = a[i][j];
-               a[i][j] = a[j][i];
-               a[j][i] = temp;
+        int m = mat.length, n = mat[0].length;
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                arr[j][i] = mat[m - 1 - i][j];
             }
         }
-    }
-     static void reverse(int matrix[][]){
-            for(int i =0; i<matrix.length ; i++){
-                int start =0 , end = matrix.length -1;
-                
-                while(start < end){
-                    int temp = matrix[start][i];
-                    matrix[start][i] = matrix[end][i];
-                    matrix[end][i] = temp ;
-                    start++;
-                    end--;
-                }
-            }
+        for (int i = 0; i < m; i++){
+            mat[i] = arr[i];
+        }
     }
 }
