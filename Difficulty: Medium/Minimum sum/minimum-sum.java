9f8144d30d9b1dabe9285bc7f3b9e52bfd7dc87a
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-11-09 20:28:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum sum/minimum-sum.java
+++ Difficulty: Medium/Minimum sum/minimum-sum.java
@@ -1,81 +1,57 @@
 //{ Driver Code Starts
-//Initial Template for Java
-
-
-
+// Initial Template for Java
 
 import java.io.*;
+import java.lang.*;
 import java.util.*;
 
-public class Main {
-
-    public static void main(String[] args) throws Exception {
+class GFG {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-        while (tc-- > 0) {
-            String[] inputLine;
-            inputLine = br.readLine().trim().split(" ");
-            int n = Integer.parseInt(inputLine[0]);
-            int[] arr = new int[n];
-            inputLine = br.readLine().trim().split(" ");
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(inputLine[i]);
-            }
+        int t = Integer.parseInt(br.readLine());
 
-            String ans = new Solution().solve(arr, n);
-            System.out.println(ans);
+        while (t-- > 0) {
+            String arr[] = br.readLine().split(" ");
+            int a[] = new int[arr.length];
+
+            for (int i = 0; i < arr.length; i++) {
+                a[i] = Integer.parseInt(arr[i]);
+            }
+            Solution obj = new Solution();
+            int f = 0;
+            String A = obj.minSum(a);
+            System.out.println(A);
+            System.out.println("~");
         }
     }
 }
-
-
 // } Driver Code Ends
 
 
-//User function Template for Java
-
-
-
-
+// User function Template for Java
 
 class Solution {
-    String solve(int[] arr, int n) {
-        StringBuilder num1 = new StringBuilder();
-        StringBuilder num2 = new StringBuilder();
-        Arrays.sort(arr);
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                num1.append(arr[i]);
-            } else {
-                num2.append(arr[i]);
+    String minSum(int[] arr) {
+        // code here
+        
+              Arrays.sort(arr);
+         StringBuilder st=new StringBuilder();
+        int carry=0;
+        for(int i=arr.length-1;i>=0;i-=2){
+            if(arr[i]+carry==0) break;
+            if(i-1>=0){
+                if(arr[i]+arr[i-1]+carry>9){
+                    st.append((arr[i]+arr[i-1]+carry)%10);
+                     carry=1;
+                }
+                else {st.append(arr[i]+arr[i-1]+carry); carry=0;}
+            }
+            else{
+                    st.append(carry+arr[i]);
+                    carry=0;
             }
         }
-
-        return removeLeadingZeros(addStrings(num1.toString(), num2.toString()));
+        if(carry>0) st.append(carry);
+        return st.reverse().toString();
     }
-
-    private String addStrings(String num1, String num2) {
-        StringBuilder result = new StringBuilder();
-        int carry = 0;
-        int i = num1.length() - 1, j = num2.length() - 1;
-        while (i >= 0 || j >= 0 || carry != 0) {
-            int x = (i >= 0) ? num1.charAt(i) - '0' : 0;
-            int y = (j >= 0) ? num2.charAt(j) - '0' : 0;
-            int sum = x + y + carry;
-            result.append(sum % 10);
-            carry = sum / 10;
-            i--;
-            j--;
-        }
-        return result.reverse().toString();
-    }
-     private String removeLeadingZeros(String str) {
-        int i = 0;
-        while (i < str.length() && str.charAt(i) == '0') {
-            i++;
-        }
-        return i == str.length() ? "0" : str.substring(i);
-    }
-
 }
-
