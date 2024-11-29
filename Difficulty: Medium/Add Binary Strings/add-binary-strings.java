f50a1c72215b1f4308cfe54095c6d0cf5a43910f
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-11-29 09:56:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Add Binary Strings/add-binary-strings.java
+++ Difficulty: Medium/Add Binary Strings/add-binary-strings.java
@@ -27,27 +27,23 @@ class GFG {
 class Solution {
     public String addBinary(String s1, String s2) {
         // code here
-     int n = s1.length();
-        int m = s2.length();
-        
-        int i = n - 1;
-        int j = m - 1;
-        StringBuilder ans = new StringBuilder();
-        int carry = 0;
-        
-        while (i >= 0 || j >= 0 || carry > 0) {
+        int m = s1.length() ; 
+        int n = s2.length(); 
+        int i = m-1; 
+        int j = n-1; 
+        StringBuilder ans= new StringBuilder();
+        int carry = 0; 
+        while(i>= 0 || j>=0 || carry > 0){
             int sum = carry;
-            
+             
             if (i >= 0) sum += s1.charAt(i) - '0';
             if (j >= 0) sum += s2.charAt(j) - '0';
             
-            ans.append(sum % 2);
-            carry = sum / 2;
-            
-            i--;
+            ans.append(sum%2);
+            carry= sum / 2;
+            i--; 
             j--;
         }
-
         
         String result = ans.reverse().toString();
         return result.replaceFirst("^0+", "");
