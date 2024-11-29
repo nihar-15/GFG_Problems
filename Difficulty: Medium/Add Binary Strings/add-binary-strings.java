a406f906870d3ea70a3800dcb21ba8b11598bd01
Niharika Kumar <133783014+nihar-15@users.noreply.github.com>
2024-11-29 09:47:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add Binary Strings/add-binary-strings.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String a, b;
+            a = sc.next();
+            b = sc.next();
+            Solution ob = new Solution();
+            System.out.println(ob.addBinary(a, b));
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public String addBinary(String s1, String s2) {
+        // code here
+     int n = s1.length();
+        int m = s2.length();
+        
+        int i = n - 1;
+        int j = m - 1;
+        StringBuilder ans = new StringBuilder();
+        int carry = 0;
+        
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+            
+            if (i >= 0) sum += s1.charAt(i) - '0';
+            if (j >= 0) sum += s2.charAt(j) - '0';
+            
+            ans.append(sum % 2);
+            carry = sum / 2;
+            
+            i--;
+            j--;
+        }
+
+        
+        String result = ans.reverse().toString();
+        return result.replaceFirst("^0+", "");
+    }
+}
